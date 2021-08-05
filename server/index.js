f1fdddd524608923f2eb1c72fb1ee81787ffa01c
Donald Chen <donaldchen@Donalds-MacBook-Pro.local>
2021-08-04 18:16:19 -0700

    add env variables to process.env

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const app = require('./app');
 
+const config = require('dotenv').config()
+
 const port = process.env.PORT || 1200; // This variable will be automatically set by Heroku if we are deploying on Heroku. If it doesn't exist, we can default to 1200 on local machine.   
 
 app.listen(port, () => {
